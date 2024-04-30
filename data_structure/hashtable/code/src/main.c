9ce85f696c1c45118f808829a2d5e3d83a992ca1
joao99sb <joaovictor665@gmail.com>
2024-04-30 00:03:08 -0300

    feat: hashtable in cpp

---
+++ data_structure/hashtable/code/src/main.c
@@ -0,0 +1,21 @@
+#include "./linked_list.h"
+
+// void menu()
+// {
+//   printf("chose option\n\n");
+//   printf("1) append a number in Linked list\n");
+//   printf("2) display Linked list elements\n");
+//   printf("3) copy array's elements in Linked list \n");
+//   printf("4) get Linked list length \n");
+//   printf("5) do a linear search for some element \n");
+//   printf("6) insert a number \n");
+//   printf("7) clean terminal \n");
+//   printf("8) delete some element \n");
+//   printf("9) close program \n");
+// }
+
+int main(int argc, char const *argv[])
+{
+
+  return 0;
+}
